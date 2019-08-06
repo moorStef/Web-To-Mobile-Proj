e35262c4a53750771bc2b800729db3a00b2b9f5c
stefani.moore <stefani.moore@digitechsystems.com>
2019-08-06 10:30:01 -0600

    added angular material components to ui

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AppModule } from '../app.module';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
